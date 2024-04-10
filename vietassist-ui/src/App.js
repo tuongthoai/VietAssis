9871b253530764fc2498efbb3f200129402d6acd
voanhquan13072003@gmail.com <voanhquan13072003@gmail.com>
2024-04-10 21:26:49 +0700

    Some changes in App.js

--- vietassist-ui/src/App.js
+++ vietassist-ui/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import MainScreen from "./components/MainScreen/MainScreen";
 import { primary } from "./theme/colors";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Box } from '@mui/material'
-import { blue } from '@mui/material/colors'
 
 const theme = createTheme({
   palette: {
